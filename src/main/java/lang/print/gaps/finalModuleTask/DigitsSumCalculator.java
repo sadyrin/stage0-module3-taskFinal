f0d9dfdb4476a8b7a9bbfb463974dce6ffdb2af6
sadyrin <sadyrin@gmail.com>
2023-01-25 22:27:22 +0200

    Done task stage0-module3-final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public  void calculateSum(int number) {
+        int sum = 0;
+        String[] str = ("" + number).split("");
+        for(String s : str){
+            sum += Integer.parseInt(s);
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String[] str = ("" + number).split("");
+        System.out.print("\n");
+        for(int i = (str.length - 1); i >= 0; i--){
+            System.out.print(str[i]);
+        }
     }
 }
