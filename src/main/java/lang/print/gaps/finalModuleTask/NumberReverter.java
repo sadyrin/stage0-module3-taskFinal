0b7479d48cf4af5228f7233e3087690fd00946a8
sadyrin <sadyrin@gmail.com>
2023-01-27 18:14:11 +0200

    Done task stage0-module3-taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -3,9 +3,9 @@ package lang.print.gaps.finalModuleTask;
 public class NumberReverter {
     public void revert(int number) {
         String[] str = ("" + number).split("");
-        System.out.print("\n");
         for(int i = (str.length - 1); i >= 0; i--){
             System.out.print(str[i]);
         }
+        System.out.print("\n");
     }
 }
